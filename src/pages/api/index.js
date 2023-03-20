4c847775dfb1a830b1dd20949d8090f9fe989e36
0xInu <calin.cretiu@gmail.com>
2023-03-20 06:29:54 +0200

    added discord

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,11 +1,31 @@
 const app = require("express")();
 const { v4 } = require("uuid");
+import { Client, GatewayIntentBits } from "discord.js";
+const DISCORD_CLIENT_SECRET = process.env.DISCORD_CLIENT_SECRET;
+const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID;
+const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
+
+const discordClient = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+discordClient.on("ready", () => {
+  console.log(`Logged in as ${discordClient.user.tag}!`);
+});
+discordClient.on("messageCreate", (message) => {
+  console.log(`${message.author.tag} :: ${message.content}`);
+});
+discordClient.login(DISCORD_BOT_TOKEN);
 
 app.get("/api", (req, res) => {
   const path = `/api/item/${v4()}`;
   res.setHeader("Content-Type", "text/html");
   res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+  res.end(`Hello! Token is: ${DISCORD_BOT_TOKEN}`);
 });
 
 app.get("/api/item/:slug", (req, res) => {
